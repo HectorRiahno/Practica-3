ca9aa58b9d8d7991a44ee7d44d17d17e97dcca13
HectorRiahno <hjrianol10@gmail.com>
2023-12-11 15:56:13 -0500

    add new fuction

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
 import { ref } from 'vue';
 let input=ref('')
+let turnos=ref(10)
+let faltantes=ref(0)
 let modelo=ref()
+
 let array1=ref([{code:'XDCHH'},
 {code:'XDCHH'},
 {code:'XDCHH'},
@@ -16,16 +19,20 @@ let array3=ref([{code:'LOJ7'},
 {code:'HUFJD'}])
 
 function add() {
+  if(input.value.trim()===''){return}
   if(modelo.value==1){
     array1.value.push({code:input.value})
+    turnos.value++
     modelo.value=''
     input.value=''
   }else if(modelo.value==2){
     array2.value.push({code:input.value})
+    turnos.value++
     modelo.value=''
     input.value=''
   }else   if(modelo.value==3){
     array3.value.push({code:input.value})
+    turnos.value++
     modelo.value=''
     input.value=''
   }
@@ -33,12 +40,48 @@ function add() {
 function borrar(i,z){
   if(z==1){
     array1.value.splice(i,1)
+    turnos.value--
+    faltan(i,z)
   }else if(z==2){
     array2.value.splice(i,1)
-  }else   if(z==3){
+    turnos.value--
+    faltan(i,z)
+  }else if(z==3){
     array3.value.splice(i,1)
+    turnos.value--
+    faltan(i,z)
   }
 }
+function faltan(i,z){
+  faltantes.value=0
+  if(z===1){
+    for(let j=0;j<array1.value.length;j++){
+      if(i!=j){
+        faltantes.value++
+      }else{
+        return
+      }
+    }
+  }else if(z===2){
+    for(let j=0;j<array2.value.length;j++){
+      if(i!=j){
+        faltantes.value++
+      }else{
+        return
+      }
+    }
+  }else if(z===3){
+    
+    for(let j=0;j<array3.value.length;j++){
+      if(i!=j){
+        faltantes.value++
+      }else{
+        return
+      }
+    }
+  }
+}
+
 </script>
 
 <template>
@@ -55,7 +98,8 @@ function borrar(i,z){
           <!-- div de carga del arreglo -->
           <div v-for="(obj,i) in array1" :key="i" class="bg-white m-3 p-1 rounded-md flex justify-between">
             <p>{{ i+1 }}-{{ array1[i].code }}</p>
-            <p @click="borrar(i,1)" class="bg-red-300 px-2 rounded-2xl">X</p>
+            <p @click="faltan(i,1)" class="bg-red-300 px-2 rounded-2xl cursor-pointer">??</p>
+            <p @click="borrar(i,1)" class="bg-red-300 px-2 rounded-2xlcursor-pointer">X</p>
           </div>
         </div>
         <div class="bg-neutral-100 w-2/5">
@@ -65,7 +109,8 @@ function borrar(i,z){
           <!-- div de carga del arreglo -->
           <div v-for="(obj,i) in array2" :key="i" class="bg-white m-3 p-1 rounded-md flex justify-between">
             <p>{{ i+1 }}-{{ array2[i].code }}</p>
-            <p @click="borrar(i,2)" class="bg-red-300 px-2 rounded-2xl">X</p>
+            <p @click="faltan(i,2)" class="bg-red-300 px-2 rounded-2xl cursor-pointer">??</p>
+            <p @click="borrar(i,2)" class="bg-red-300 px-2 rounded-2xl cursor-pointer">X</p>
           </div>
         </div>
         <div class="bg-neutral-100 w-2/5">
@@ -75,13 +120,15 @@ function borrar(i,z){
           <!-- div de carga del arreglo -->
           <div v-for="(obj,i) in array3" :key="i" class="bg-white m-3 p-1 rounded-md flex justify-between">
             <p>{{ i+1 }}-{{ array3[i].code }}</p>
-            <p @click="borrar(i,3)" class="bg-red-300 px-2 rounded-2xl">X</p>
+            <p @click="faltan(i,3)" class="bg-red-300 px-2 rounded-2xl cursor-pointer">??</p>
+            <p @click="borrar(i,3)" class="bg-red-300 px-2 rounded-2xl cursor-pointer">X</p>
           </div>
         </div>
       </div>
       <div class="px-4 bg-neutral-200 mt-4 flex justify-evenly py-5">
         <!-- variable q se actualiza -->
-        <h1>9 turnos</h1>
+        <h1>{{ turnos }} turnos</h1>
+        <h1>te faltan {{ faltantes }} turno</h1>
         <input v-model="input" class="px-2 " placeholder="Código" type="text">
         <input v-model="modelo" type="number" placeholder="de 1-3">
         <p @click="add" class="text-white bg-violet-400 p-1 px-6 rounded-lg">Agregar</p>
